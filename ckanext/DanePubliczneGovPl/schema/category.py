27529b856d21b855976eb2acdeadb71ff67e825b
KrzysztofMadejski <krzysztof@madejscy.pl>
2015-03-03 15:37:41 +0100

    [#15] Categories

--- ckanext/DanePubliczneGovPl/schema/category.py
+++ ckanext/DanePubliczneGovPl/schema/category.py
@@ -1,3 +1,4 @@
+import ckan.lib
 import ckan.plugins as p
 import ckan.plugins.toolkit as tk
 
@@ -8,10 +9,24 @@ class Category(p.SingletonPlugin):
     p.implements(p.ITemplateHelpers)
 
     def get_helpers(self):
-        return {'dp_categories': self.categories,
+        return {'dp_categories': self.h_categories,
+                'dp_category_colors': self.h_colors,
             }
 
-    def categories(self):
+    def h_categories(self):
         categories = tk.get_action('group_list')(data_dict={'all_fields': True})
 
         return categories
+
+    def h_colors(self):
+        return [
+            '#4b77be',
+            '#24485f',
+            '#6c7a89',
+            '#674172',
+            '#bf3607',
+            '#3a539b',
+            '#d35400',
+            '#2574a9',
+            '#138435'
+        ]
