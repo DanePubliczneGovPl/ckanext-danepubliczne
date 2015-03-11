13f53f17c4c5954d965230c57eb783e85792dd00
KrzysztofMadejski <krzysztof@madejscy.pl>
2015-03-11 12:27:28 +0100

    #34

--- ckanext/DanePubliczneGovPl/schema/category.py
+++ ckanext/DanePubliczneGovPl/schema/category.py
@@ -32,3 +32,38 @@ class Category(p.SingletonPlugin):
             '#2574a9',
             '#138435'
         ]
+
+    p.implements(p.IRoutes, inherit=True)
+    def before_map(self, map):
+        from routes.mapper import SubMapper
+        with SubMapper(map, controller='group') as m:
+            m.connect('group_index', '/category', action='index',
+                      highlight_actions='index search')
+            m.connect('group_list', '/category/list', action='list')
+            m.connect('group_new', '/category/new', action='new')
+            m.connect('group_action', '/category/{action}/{id}',
+                      requirements=dict(action='|'.join([
+                          'edit',
+                          'delete',
+                          'member_new',
+                          'member_delete',
+                          'history',
+                          'followers',
+                          'follow',
+                          'unfollow',
+                          'admins',
+                          'activity',
+                      ])))
+            m.connect('group_about', '/category/about/{id}', action='about',
+                      ckan_icon='info-sign'),
+            m.connect('group_edit', '/category/edit/{id}', action='edit',
+                      ckan_icon='edit')
+            m.connect('group_members', '/category/members/{id}', action='members',
+                      ckan_icon='group'),
+            m.connect('group_activity', '/category/activity/{id}/{offset}',
+                      action='activity', ckan_icon='time'),
+            m.connect('group_read', '/category/{id}', action='read',
+                      ckan_icon='sitemap')
+            
+        return map
+        
