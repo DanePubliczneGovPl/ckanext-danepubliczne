21444c7788f46985df0d83f4fe86090105da7c60
Daniel Macyszyn <daniel.macyszyn@epf.org.pl>
2016-09-01 10:56:04 -0700

    cleanup

--- ckanext/danepubliczne/schema/article.py
+++ ckanext/danepubliczne/schema/article.py
@@ -129,15 +129,13 @@ class Article(p.SingletonPlugin, tk.DefaultDatasetForm):
         }
 
 def _package_create(context, data_dict=None):
-    return {'success': True}
     user = context['user']
     package = context.get('package')  # None for new
 
     if package and package['type'] == 'article':
         return {'success': False, 'msg': _('User %s not authorized to create articles') % user}
-    return {'success': True}
-    return auth.create.package_create(context, data_dict)
 
+    return auth.create.package_create(context, data_dict)
 
 def _package_delete(context, data_dict=None):
     user = context['user']
