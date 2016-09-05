133ffbb3747c24b1838a6019844bcfd24221048a
Daniel Macyszyn <daniel.macyszyn@epf.org.pl>
2016-09-05 06:19:09 -0700

    Możliwość dodawania zbiorów danych do aplikacji zgłaszanych przez użytkowników #243

--- ckanext/danepubliczne/fanstatic/app-form.js
+++ ckanext/danepubliczne/fanstatic/app-form.js
@@ -1,93 +1,3 @@
-var DatasetPicker = function(div) {
-	this.init(div);
-}
-
-$.extend(DatasetPicker.prototype, {
-	init: function(div) {
-		
-		this.div = $(div);
-		this.placeholder = this.div.data('placeholder');
-		
-		this.build();
-		
-		var init_value = this.div.data('value');
-		if( init_value ) {
-			
-			var values = init_value.split('&');
-			for( var i=0; i<values.length; i++ ) {
-				this.addInput(values[i]);
-			}
-			
-		}
-		
-		console.log(init_value);
-		
-	},
-	build: function() {
-		
-		this.inputs_div = $('<div>').addClass('inputs');
-		this.controlls_div = $('<div>').addClass('controlls');
-		
-		var that = this;
-		
-		var a = $('<a>').addClass('add').attr('href', '#').text('Dodaj zbiór danych').click(function(event){
-			event.preventDefault();
-			that.addInput(false);
-		});
-		
-		this.div.append(this.inputs_div).append(this.controlls_div.append(a));
-		
-	},
-	addInput: function(value) {
-		
-		var inp = $('<input>');
-		inp.attr('type', 'text').attr('name', 'dataset_name').attr('placeholder', this.placeholder);
-		
-		if( value )
-			inp.val( value );
-		else
-			inp.val('');
-		
-		var that = this;
-		
-		var controlls = $('<div>').addClass('controlls');
-		var remove_btn = $('<a>').attr('href', '#').addClass('remove').click(function(event){
-			event.preventDefault();
-			that.removeInput(inp);
-		});
-		controlls.append(remove_btn);
-		
-		var inp_div = $('<div>').addClass('inp_group');	
-		var inp_cont = $('<div>').addClass('inp_cont').append(inp);
-		
-		inp_div.append(inp_cont).append(controlls);
-		this.inputs_div.append(inp_div);
-		
-		if( !value )
-			inp.focus();
-				
-	},
-	removeInput: function(inp) {
-		
-		var inp_group = inp.parents('.inp_group');
-		
-		if( inp_group.length ) {
-			
-			var inp = inp_group.find('input');
-			var v = $.trim( inp.val() );
-						
-			if( v ) {
-				if( confirm('Czy na pewno chcesz usunąć ten zbiór danych?') )
-					inp_group.remove();
-			} else {
-				inp_group.remove();
-			}
-			
-		}
-		
-	}
-});
-
 function today() {
 	var today = new Date();
 	var dd = today.getDate();
@@ -107,11 +17,8 @@ function today() {
 }
 
 $(document).ready(function(){
-	
-	dp = new DatasetPicker('.datasets_picker');
-	
+		
 	var date_inp = $('#field-date');
-	
 	if( date_inp.length ) {
 		
 		var v = $.trim(date_inp.val());
@@ -128,5 +35,3 @@ $(document).ready(function(){
 	}
 	
 });
-
-var dp;
--- ckanext/danepubliczne/fanstatic/danepubliczne.js
+++ ckanext/danepubliczne/fanstatic/danepubliczne.js
@@ -1,3 +1,93 @@
+var DatasetPicker = function(div) {
+	this.init(div);
+}
+
+$.extend(DatasetPicker.prototype, {
+	init: function(div) {
+		
+		this.div = $(div);
+		if( !this.div.length )
+			return false;
+			
+		this.placeholder = this.div.data('placeholder');
+		
+		this.build();
+		
+		var init_value = this.div.data('value');
+		if( init_value ) {
+			
+			var values = init_value.split('&');
+			for( var i=0; i<values.length; i++ ) {
+				this.addInput(values[i]);
+			}
+			
+		}
+		
+		console.log(init_value);
+		
+	},
+	build: function() {
+		
+		this.inputs_div = $('<div>').addClass('inputs');
+		this.controlls_div = $('<div>').addClass('controlls');
+		
+		var that = this;
+		
+		var a = $('<a>').addClass('add').attr('href', '#').text('Dodaj zbiór danych').click(function(event){
+			event.preventDefault();
+			that.addInput(false);
+		});
+		
+		this.div.append(this.inputs_div).append(this.controlls_div.append(a));
+		
+	},
+	addInput: function(value) {
+		
+		var inp = $('<input>');
+		inp.attr('type', 'text').attr('name', 'dataset_name').attr('placeholder', this.placeholder);
+		
+		if( value )
+			inp.val( value );
+		else
+			inp.val('');
+		
+		var that = this;
+		
+		var controlls = $('<div>').addClass('controlls');
+		var remove_btn = $('<a>').attr('href', '#').addClass('remove').click(function(event){
+			event.preventDefault();
+			that.removeInput(inp);
+		});
+		controlls.append(remove_btn);
+		
+		var inp_div = $('<div>').addClass('inp_group');	
+		var inp_cont = $('<div>').addClass('inp_cont').append(inp);
+		
+		inp_div.append(inp_cont).append(controlls);
+		this.inputs_div.append(inp_div);
+				
+	},
+	removeInput: function(inp) {
+		
+		var inp_group = inp.parents('.inp_group');
+		
+		if( inp_group.length ) {
+			
+			var inp = inp_group.find('input');
+			var v = $.trim( inp.val() );
+						
+			if( v ) {
+				if( confirm('Czy na pewno chcesz usunąć ten zbiór danych?') )
+					inp_group.remove();
+			} else {
+				inp_group.remove();
+			}
+			
+		}
+		
+	}
+});
+
 // cookies
 if (!Cookies.get('kukiz_accept_cookies')) {
   $(function() {
@@ -57,3 +147,8 @@ $('a[href="#field-sitewide-search"]').click(function(e){
     document.getElementById("field-sitewide-search").focus();
     return false;
 })
+
+$(document).ready(function(){
+	_dp = new DatasetPicker('.datasets_picker');
+});
+var _dp;
