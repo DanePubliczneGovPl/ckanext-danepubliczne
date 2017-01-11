1c47a6e7fe47c3d83d891b1716257896eee22ab0
Krzysiek Madejski <krzysztof.madejski@epf.org.pl>
2017-01-11 21:53:39 +0100

    Create restore.sh

---
+++ scripts/restore.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+remote_user=ckan-v5
+backup_user=remote-ckan1
+backup_server=<BACKUP_SERVER>
+
+echo "Starting retrieving backup at $(date)"
+
+apachectl stop
+supervisorctl stop celery
+
+sudo -u postgres dropdb ckan
+# sudo -u postgres dropdb datastore
+sudo -u postgres createdb -O ckan ckan -E utf-8
+# sudo -u postgres createdb -O ckan datastore -E utf-8
+
+echo "Retrieving latest backup.."
+
+sudo -u ckan ssh $remote_user@$backup_server -C "cat /home/backup/last/$backup_user-ckan.pgdump" | sudo -u postgres pg_restore -d ckan
+
+apachectl start
+supervisorctl start celery
+
+echo "Rebuilding search index.."
+
+su ckan
+workon ckan
+paster --plugin=ckan search-index rebuild
+
+echo "Finished at $(date)"
+
+# Be sure to have identical beaker.session.secret and app_instance_uuid configuration variables
+# Be sure to have the same PIP packages installed (do freeze and req install)
